2fe3cf94eefa05778a05abbbec135dfd39b4929f
MikeT <mikejamesthompson@gmail.com>
2014-01-07 13:35:27 +0000

    Upadting the config example to reflect previous commit's changes

--- config-example.py
+++ config-example.py
@@ -1,14 +1,8 @@
-# Maps staff ids in Highrise to their names
-STAFF = {'111111':'Bob',
-		'222222':'Fred'}
-
 # API authentication token
 AUTH_TOKEN = '123456789abcdef123456789abcdef12'
 
-# Maps category ids in highrise to their names
-CATEGORIES = {"1111111" : "Product 1",
-			"2222222" : "Service 1",
-			"3333333" : "Product 2"}
-
 # User Agent string - used so that 37 Signals can get in touch if necessary
 USER_AGENT = "Application name (name@example.com)"
+
+# Domain - the URL for your instance of Highrise
+DOMAIN = "https://instance.highrisehq.com"
